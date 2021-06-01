0eba079875f9b369946c6fd501e2a64a13580e0e
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-06-01 14:10:47 +0530

    Intial Commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// Importing Helpers
+import React, { useState } from "react";
+
+// Importing Components
+import SingleColour from "./SingleColour";
+// import Values from 'values.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">Test</div>;
 }
 
+// Default Export
 export default App;
---
+++ src/SingleColour.js
@@ -0,0 +1,11 @@
+// Importing Helpers
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./Utils";
+
+// Functional Component
+const SingleColour = () => {
+  return <div></div>;
+};
+
+// Default Export
+export default SingleColour;
