7099243f3e82f74fb5661f5d20b98d53a0cd3000
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-06-02 11:38:41 +0530

    Created the structure.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,37 @@ import React, { useState } from "react";
 
 // Importing Components
 import SingleColour from "./SingleColour";
-// import Values from 'values.js';
+// import Values from "values.js";
 
 function App() {
-  return <div className="App">Test</div>;
+  const [color, setColor] = useState("");
+  const [error, setError] = useState("False");
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Hello");
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>Color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+          />
+          <button className="btn" type="submit">Submit</button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>List Goes Here</h4>
+      </section>
+    </>
+  );
 }
 
 // Default Export
