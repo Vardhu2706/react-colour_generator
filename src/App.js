a2b5094cfb67f1e4aa1d76147db9042b77bdf3e9
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-06-02 13:53:28 +0530

    Added Copy to Clipboard functionality.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Values from "values.js";
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#0099ff").all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -30,7 +30,7 @@ function App() {
             type="text"
             value={color}
             onChange={(e) => setColor(e.target.value)}
-            placeholder="#f15025"
+            placeholder="#0099FF"
             className={`${error ? "error" : null}`}
           />
           <button className="btn" type="submit">
